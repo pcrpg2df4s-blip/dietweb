b6d396069a51b10d7e061e788cfb95a82b8a6be6
Nak1112 <savelynaku@gmail.com>
2026-01-29 12:05:52 +0100

    UI: removed Б, У, Ж labels from macro cards (v1.1.25)

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ console.log("Debug: API Key:", apiKeyFromUrl ? "Present (Starts with " + apiKeyF
 
 const CONFIG = {
     GOOGLE_API_KEY: apiKeyFromUrl || "",
-    VERSION: "1.1.24"
+    VERSION: "1.1.25"
 };
 
 if (!CONFIG.GOOGLE_API_KEY) {
