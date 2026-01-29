e4e6015b6ae66f179c0ac78e12797bdb94133c99
Nak1112 <savelynaku@gmail.com>
2026-01-30 00:26:38 +0100

    Bump version to 1.1.35

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ console.log("Debug: API Key:", apiKeyFromUrl ? "Present (Starts with " + apiKeyF
 
 const CONFIG = {
     GOOGLE_API_KEY: apiKeyFromUrl || "",
-    VERSION: "1.1.34"
+    VERSION: "1.1.35"
 };
 
 if (!CONFIG.GOOGLE_API_KEY) {
