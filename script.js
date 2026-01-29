adb6c2b6a6d96d75bfe8e7a4924fa1ab6888388b
Nak1112 <savelynaku@gmail.com>
2026-01-29 01:28:20 +0100

    UI: updated tab bar with SVG icons, removed text labels, and bumped version to 1.1.15

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ console.log("Debug: API Key:", apiKeyFromUrl ? "Present (Starts with " + apiKeyF
 
 const CONFIG = {
     GOOGLE_API_KEY: apiKeyFromUrl || "",
-    VERSION: "1.1.13"
+    VERSION: "1.1.15"
 };
 
 if (!CONFIG.GOOGLE_API_KEY) {
